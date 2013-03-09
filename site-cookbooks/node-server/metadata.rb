1d07a9bdc0d725c9c567cd9ee0e90d87d58c95e8
Keisuke SATO <riaf@me.com>
2013-03-09 20:33:58 +0900

    Add node-server

---
+++ site-cookbooks/node-server/metadata.rb
@@ -0,0 +1,7 @@
+name             'node-server'
+maintainer       'Keisuke SATO'
+maintainer_email 'riaf@me.com'
+license          'All rights reserved'
+description      'Installs/Configures node-server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ site-cookbooks/node-server/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: node-server
+# Recipe:: default
+#
+# Copyright 2013, Keisuke SATO
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "npm"
+
+npm_package "grunt-cli@0.1.6"
+
